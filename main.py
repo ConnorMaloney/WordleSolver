ce048e5461d86296ac4825463712f60dead642c9
Connor Maloney <maloneydev@outlook.com>
2022-01-29 13:35:14 -0500

    REDO: Found the actual word lists from source code

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-# Wordle solver for 1/24/2022, World 219
-import random 
+#12972 possible words
+#10657 guess words
+#2315 valid words 
 
-# Read file with valid five letter words
-with open("fiveLetterWords.txt", "r") as f:
-    file = f.readlines()
+# Wordle solver
 
-# Old way was brute force, now I'm going to iterate over only valid words
-letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'p', 'a', 's', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
-words = []
+# Stage 1: Have it successfully guess a word given certain parameters - COMPLETE
+# Stage 2: Have it interpret what the most common letters in 5 letter words are (use list from english word list) - IN PROCESS
+# Stage 3: Clean up code, make it more modular
+# Stage 4: Have it fully autonomous and hooked up to online, guessing itself
 
-# Just for converting strings
-def convert(s):
-    # initialization of string to ""
-    str1 = ""
-    # using join function join the list s by 
-    # separating words by str1
-    return(str1.join(s))
-
-for word in file:
-    if word.find('a') == 3 and word.find('u') == 1 and "i" not in word and "o" not in word and "d" not in word and "h" not in word and "n" not in word and "m" not in word:
-        print(word)
-
+from wordleDict import validWords
+        
+print(*validWords)
+print("Possible words: ", len(validWords))
